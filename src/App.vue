6b0fa86f671c6a351e7564b62c03234f07fd918e
Mario P <MPeusschers@ragingriver.net>
2018-07-02 15:10:29 +0200

    New way of loading seasons

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <input id="input-search" type="text" class="form-control" v-model="textSearch" placeholder='Year'>
+    <input id="input-search" type="text" class="form-control" v-model="winYear" placeholder='Year'>
     <ul>
-      <template v-for="item in startFrom(yearFilter, 0)">
-        <li>{{ item.season }}</li>
-      </template>
-    </ul>
-    <ul>
-      <template v-for="item in winners.StandingsLists" v-if="inRange(item.season)">
-        <li>{{ item.season }}</li>
-        <br />
-        <br />
-      </template>
+      <!-- <template v-for="item in winners.StandingsLists" v-if="inRange(item.season)">
+        <ul>
+          <li> -->
+            <!-- <h3>{{ item.season }}</h3> -->
+            <template
+              v-for="item in driverDetail"
+              :class="{ 'active': item[0] }"
+              :text="item">
+              {{ item }}
+            </template>
+          <!-- </li>
+        </ul>
+      </template> -->
     </ul>
   </div>
 </template>
@@ -26,39 +29,28 @@ export default {
   data () {
     return {
       msg: 'F1 Champions Collection',
-      seasons: [],
+      drivers: [],
+      details: [],
       winners: [],
-      textSearch: "",
       winYear: "",
     }
   },
   computed: {
      yearFilter: function() {
        let textSearch = this.textSearch;
-       return this.seasons.filter(function(el) {
-         return el.season.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1;
-       });
-     },
-     winFilter: function() {
-       let textSearch = this.textSearch;
        return this.winners.filter(function(el) {
          return el.season.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1;
        });
+     },
+     driverDetail: function () {
+        for(let j = 0; j < this.winners.StandingsLists.length; j++){
+          this.drivers.push({season: this.winners.StandingsLists[j].season, winner: this.winners.StandingsLists[j].DriverStandings[0].Driver.givenName});
+        }
+      console.log(this.drivers);
+      return this.drivers;
      }
   },
   mounted() {
-    // Get Seasons
-      axios.get("http://ergast.com/api/f1/seasons.json", {
-        params: {
-          limit: "100",
-          offset: "55"
-        }
-      }).then(result => {
-          this.seasons = result.data.MRData.SeasonTable.Seasons;
-      }, error => {
-          console.error(error);
-      });
-
       //Get Champions
       const winner = 1;
       let winYear = this.winYear;
@@ -70,6 +62,7 @@ export default {
         }
       }).then(result => {
             this.winners = result.data.MRData.StandingsTable;
+            this.details = this.winners.StandingsLists;
         }, error => {
             console.error(error);
         });
@@ -83,14 +76,12 @@ export default {
     inRange: function(year) {
       var i;
       for (i = 0; i < year.length; i++) {
-        console.log(year);
         if (year >= "2005" && year <= "2015") {
-          return "Yes";
+          return true;
         break;
         }
       }
-
-      return "";
+      return false;
     }
     
   }
